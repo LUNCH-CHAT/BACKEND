4a7ef15e944f7e31806604b517aff0b20a4ee0e7
Dobby19 <southvi1@naver.com>
2025-07-03 10:20:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/lunchchat/LaunchatApplication.java
@@ -0,0 +1,13 @@
+package com.lunchchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LaunchatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lunchchat/LaunchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
