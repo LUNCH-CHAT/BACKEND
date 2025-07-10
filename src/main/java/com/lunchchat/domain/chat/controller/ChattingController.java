bc43e6724d9ec73244044612faefe73b11cfe769
lsw71311 <lsw71311@naver.com>
2025-07-10 17:55:05 +0900

    [FEAT] 채팅 메시지 전송 api

--- src/main/java/com/lunchchat/domain/chat/controller/ChattingController.java
+++ src/main/java/com/lunchchat/domain/chat/controller/ChattingController.java
@@ -1,5 +1,7 @@
 package com.lunchchat.domain.chat.controller;
 
+import com.lunchchat.domain.chat.dto.request.ChatMessageReq;
+import com.lunchchat.domain.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -9,10 +11,13 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class ChattingController {
 
-    //메시지 전송
-//    @MessageMapping("/{roomId}")
-//    public void sendMessage(@DestinationVariable Long roomId, MessageSendReq messageSendReq){
-//
-//    }
+    private final ChatMessageService chatMessageService;
+
+    // 클라이언트로부터 채팅 메시지 받는 엔드포인트
+    @MessageMapping("/chat/{roomId}")
+    public void handleMessage(@DestinationVariable Long roomId, ChatMessageReq chatMessageReq) {
+        // 채팅 메시지 전송
+        chatMessageService.sendMessage(roomId, chatMessageReq);
+    }
 
 }
---
+++ src/main/java/com/lunchchat/domain/chat/service/ChatMessageService.java
@@ -0,0 +1,50 @@
+package com.lunchchat.domain.chat.service;
+
+import com.lunchchat.domain.chat.chat_message.entity.ChatMessage;
+import com.lunchchat.domain.chat.chat_room.entity.ChatRoom;
+import com.lunchchat.domain.chat.dto.request.ChatMessageReq;
+import com.lunchchat.domain.chat.dto.response.ChatMessageRes;
+import com.lunchchat.domain.chat.repository.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    public final ChatMessageRepository chatMessageRepository;
+    public final SimpMessageSendingOperations messagingTemplate;
+
+    // 메시지 전송 로직 구현
+
+    public void sendMessage(Long roomId, ChatMessageReq messageReq) {
+
+        //user, room 불러오기 -> 유저 구현시 직접 참조로 변경
+        Long senderId = messageReq.senderId();
+        ChatRoom room = chatMessageRepository.findById(roomId)
+                .orElseThrow(() -> new IllegalArgumentException("채팅방이 존재하지 않습니다.")).getChatRoom();
+
+        ChatMessage message = chatMessageRepository.save(handleMessage(senderId, room, messageReq.content()));
+
+        ChatMessageRes chatMessageRes = ChatMessageRes.of(roomId, message);
+
+        messagingTemplate.convertAndSend("/topic/chat/" + roomId, chatMessageRes);
+
+        //알림 로직 구현시 추가
+    }
+
+    private ChatMessage handleMessage(Long senderId, ChatRoom room, String content) {
+
+        if (senderId.equals(room.getStarterId())) {
+            room.activateRoom();
+            return ChatMessage.of(room, senderId, content);
+        } else if (senderId.equals(room.getFriendId())) {
+            room.activateRoom();
+            return ChatMessage.of(room, senderId, content);
+        } else
+        {
+            throw new IllegalArgumentException("Invalid sender ID");
+        }
+    }
+}
