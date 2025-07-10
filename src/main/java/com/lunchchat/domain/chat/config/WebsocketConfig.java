eec1a34096ac2de420ae42099f31ee4abc388070
lsw71311 <lsw71311@naver.com>
2025-07-11 03:25:26 +0900

    [FIX] websocket 접속 오류로 인한 엔드포인트 추가

--- src/main/java/com/lunchchat/domain/chat/config/WebsocketConfig.java
+++ src/main/java/com/lunchchat/domain/chat/config/WebsocketConfig.java
@@ -16,13 +16,21 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws") // STOMP 엔드포인트 설정
                 .setAllowedOrigins("*") // CORS 설정, 실제 도메인으로 변경 필요
-                .withSockJS();
+                .withSockJS(); // 브라우저가 웹소켓 지원하지 않을시 sockJs 방식으로 연결
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
     }
 
     // 메시지 브로커 설정
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //메시지를 받을 때 경로를 설정
+        //해당 경로로 simpleBroker 등록
+        //구독하는 클라이언트에게 메시지 전달
         registry.enableSimpleBroker("/sub");
+
+        //메시지 전송 시 경로 설정 / send 요청 처리
+        //클라이언트가 메시지를 보낼 때, 경로 앞에 /pub 있으면 broker로 보내짐
         registry.setApplicationDestinationPrefixes("/pub");
     }
 
