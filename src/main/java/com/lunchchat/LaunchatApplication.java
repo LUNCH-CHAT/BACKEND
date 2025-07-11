8a2f31ca450557071de9bcb1deb10f34d48efefc
jinseo4829 <chjs9284@naver.com>
2025-07-11 17:33:08 +0900

    yml 원래대로

--- src/main/java/com/lunchchat/LaunchatApplication.java
+++ src/main/java/com/lunchchat/LaunchatApplication.java
@@ -2,8 +2,10 @@ package com.lunchchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.lunchchat.domain")
 public class LaunchatApplication {
 
   public static void main(String[] args) {
