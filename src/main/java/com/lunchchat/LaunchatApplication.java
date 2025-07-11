c1a0520217b504753cc89751ac2c643c2fa3101b
jinseo4829 <chjs9284@naver.com>
2025-07-11 17:34:21 +0900

    원상복귀

--- src/main/java/com/lunchchat/LaunchatApplication.java
+++ src/main/java/com/lunchchat/LaunchatApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.lunchchat.domain")
 public class LaunchatApplication {
 
   public static void main(String[] args) {
