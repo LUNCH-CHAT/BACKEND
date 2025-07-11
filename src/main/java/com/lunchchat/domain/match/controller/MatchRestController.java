3b514001033c840b57260483c7bcb58d95c1f661
haerxeong <haeryeong04@naver.com>
2025-07-11 14:07:05 +0900

    [FEAT] 매치 수락/거절 API 구현

--- src/main/java/com/lunchchat/domain/match/controller/MatchRestController.java
+++ src/main/java/com/lunchchat/domain/match/controller/MatchRestController.java
@@ -16,6 +16,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,9 +46,32 @@ public class MatchRestController {
   }
 
   @PostMapping
-  @Operation(summary = "매치 요청", description = "매치 요청을 생성합니다. 현재는 테스트용으로 하드코딩된 사용자 ID를 사용합니다.")
+  @Operation(summary = "매치 요청", description = "매치 요청을 생성합니다.")
   public ApiResponse<MatchResponseDto.MatchResultDto> createMatchRequest(@RequestBody Long toMemberId) {
+    // TODO: 인증 유저로 교체
     Matches match = matchCommandService.requestMatch(1L, toMemberId);
     return ApiResponse.onSuccess(MatchConverter.toMatchResultDto(match));
+  }
+
+  @PatchMapping("/{id}/accept")
+  @Operation(summary = "매칭 수락", description = "특정 매칭 요청을 수락합니다.")
+  public ApiResponse<Void> acceptMatch(@PathVariable Long id) {
+
+    // TODO: 인증 유저로 교체
+    Long currentUserId = 1L;
+
+    matchCommandService.acceptMatch(id, currentUserId);
+    return ApiResponse.onSuccess(null);
+  }
+
+  @PatchMapping("/{id}/reject")
+  @Operation(summary = "매칭 거절", description = "특정 매칭 요청을 거절합니다.")
+  public ApiResponse<Void> rejectMatch(@PathVariable Long id) {
+
+    // TODO: 인증 유저로 교체
+    Long currentUserId = 1L;
+
+    matchCommandService.rejectMatch(id, currentUserId);
+    return ApiResponse.onSuccess(null);
   }
 }
