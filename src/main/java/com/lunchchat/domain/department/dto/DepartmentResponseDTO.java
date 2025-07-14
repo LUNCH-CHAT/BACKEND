f7f7975783a84f575c2f986e3f20b4fbb93cf7e2
jinseo4829 <chjs9284@naver.com>
2025-07-15 02:51:17 +0900

    [FEAT] 특정 단과대의 학과 목록 조회

---
+++ src/main/java/com/lunchchat/domain/department/dto/DepartmentResponseDTO.java
@@ -0,0 +1,11 @@
+package com.lunchchat.domain.department.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DepartmentResponseDTO {
+    private Long id;
+    private String name;
+}
