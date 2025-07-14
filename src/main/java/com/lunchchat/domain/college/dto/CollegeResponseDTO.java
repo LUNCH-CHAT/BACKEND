50ff4315b09b26c22277104482d6dddbe3e9039c
jinseo4829 <chjs9284@naver.com>
2025-07-15 02:21:40 +0900

    [FEAT] 단과대 목록 조회

---
+++ src/main/java/com/lunchchat/domain/college/dto/CollegeResponseDTO.java
@@ -0,0 +1,12 @@
+package com.lunchchat.domain.college.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CollegeResponseDTO {
+    private Long id;
+    private String name;
+}
+
---
+++ src/main/java/com/lunchchat/domain/college/service/CollegeQueryService.java
@@ -0,0 +1,9 @@
+package com.lunchchat.domain.college.service;
+
+import com.lunchchat.domain.college.dto.CollegeResponseDTO;
+
+import java.util.List;
+
+public interface CollegeQueryService {
+    List<CollegeResponseDTO> getColleges();
+}
