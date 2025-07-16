dc422ad0a4c02e3de0f415da6daacf6156145735
lsw71311 <lsw71311@naver.com>
2025-07-17 02:18:03 +0900

    [REFACTOR] 예외 메시지 전달 추가

--- src/main/java/com/lunchchat/global/apiPayLoad/exception/handler/ChatException.java
+++ src/main/java/com/lunchchat/global/apiPayLoad/exception/handler/ChatException.java
@@ -1,6 +1,7 @@
-package com.lunchchat.global.apiPayLoad.exception;
+package com.lunchchat.global.apiPayLoad.exception.handler;
 
 import com.lunchchat.global.apiPayLoad.code.BaseErrorCode;
+import com.lunchchat.global.apiPayLoad.exception.GeneralException;
 
 public class ChatException extends GeneralException {
 
