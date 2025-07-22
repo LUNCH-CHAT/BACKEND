be7ef9f058fa48ff264b44015452ee056dd2eb5a
Dobby19 <southvi1@naver.com>
2025-07-22 17:21:46 +0900

    [FEAT] RefreshToken 구현체 작성

---
+++ src/main/java/com/lunchchat/global/security/jwt/redis/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.lunchchat.global.security.jwt.redis;
+
+import java.time.Duration;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository {
+
+  void save(String key, String token, Duration ttl);
+  Optional<String> findByKey(String key);
+  void delete(String key);
+  boolean isValid(String key, String token);
+  void rotate(String key,String newToken, Duration ttl);
+}
