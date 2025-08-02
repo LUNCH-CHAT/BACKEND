7dc3b7022afcdea8bb89f42afda8d88cd0fc0fa4
Dobby19 <southvi1@naver.com>
2025-08-02 23:51:23 +0900

    [FEAT] 멘토 신청 데이터 작성 기능 구현

--- src/main/java/com/lunchchat/domain/match/controller/MentorController.java
+++ src/main/java/com/lunchchat/domain/match/controller/MentorController.java
@@ -1,5 +1,11 @@
 package com.lunchchat.domain.match.controller;
 
+import com.lunchchat.domain.match.dto.MentorDTO;
+import com.lunchchat.domain.match.service.MentorService;
+import com.lunchchat.global.apiPayLoad.ApiResponse;
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/mentor")
 public class MentorController{
 
+  private final MentorService mentorService;
 
+  public MentorController(MentorService mentorService) {
+    this.mentorService = mentorService;
+  }
+
+  @PostMapping("/monthlyM")
+  public ApiResponse<String> submit(@RequestBody MentorDTO.MonthlyMentorDTO dto) {
+    mentorService.appendRow(List.of(
+        dto.phone(),
+        dto.question(),
+        dto.university(),
+        dto.email(),
+        dto.department(),
+        dto.studentNo(),
+        dto.name()
+    ));
+    return ApiResponse.onSuccess("엑셀 작성 완료");
+  }
 
 }
---
+++ src/main/java/com/lunchchat/domain/match/service/MentorService.java
@@ -0,0 +1,47 @@
+package com.lunchchat.domain.match.service;
+
+import com.google.api.services.sheets.v4.Sheets;
+import java.io.IOException;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MentorService {
+
+  private final Sheets sheets;
+
+  public MentorService(Sheets sheets) {
+    this.sheets = sheets;
+  }
+
+  @Value("${google.sheet.id}")
+  private String sheetId;
+
+  //
+  public void appendRow(List<Object> rowData) {
+    ValueRange body = createValueRange(rowData);
+    // 엑셀 내 "이달의멘토" 탭에 작성
+    writeToSheet("이달의멘토", body);
+  }
+
+  //2차원 리스트로 변경
+  private ValueRange createValueRange(List<Object> rowData) {
+    return new ValueRange()
+        .setValues(List.of(rowData));
+  }
+
+  //구글 시트에 값 추가
+  private void writeToSheet(String sheetName, ValueRange valueRange) {
+    try {
+      sheets.spreadsheets().values()
+          //!A1 = 시작점, valueRange = 데이터
+          .append(sheetId, sheetName + "!A1", valueRange)
+          .setValueInputOption("USER_ENTERED")
+          .execute();
+    } catch (IOException e) {
+      throw new RuntimeException("엑셀에 추가하지 못했습니다", e);
+    }
+  }
+}
