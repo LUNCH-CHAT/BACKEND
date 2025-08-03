4bb1d7fdfc5c4dd1e237ad5fcb0707fa88f1821b
margie1a <4987kk@naver.com>
2025-08-03 14:52:38 +0900

    [DEPLOY] 구글 시트 환경변수 추가

--- src/main/java/com/lunchchat/global/config/security/GoogleSheetsConfig.java
+++ src/main/java/com/lunchchat/global/config/security/GoogleSheetsConfig.java
@@ -5,30 +5,34 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class GoogleSheetsConfig {
 
-  @Bean
-  public Sheets sheetsService() throws Exception {
+    @Value("${GOOGLE_SHEETS_SERVICE_ACCOUNT_FILE}")
+    private String serviceAccountFilePath;
 
-    // 인증파일
-    InputStream input = new ClassPathResource("sheets-key.json").getInputStream();
+    @Bean
+    public Sheets sheetsService() throws Exception {
 
-    // 인증객체 생성
-    GoogleCredentials credentials = GoogleCredentials.fromStream(input)
-        .createScoped(List.of("https://www.googleapis.com/auth/spreadsheets"));
+        // 인증파일 (FCM과 동일한 방식으로 환경변수에서 파일 경로 읽기)
+        InputStream input = new FileInputStream(serviceAccountFilePath);
 
-    // 클라이언트 빌더 생성
-    return new Sheets.Builder(
-        GoogleNetHttpTransport.newTrustedTransport(),
-        JacksonFactory.getDefaultInstance(),
-        new HttpCredentialsAdapter(credentials)
-    ).setApplicationName("런치챗_이달의멘토").build();
-  }
+        // 인증객체 생성
+        GoogleCredentials credentials = GoogleCredentials.fromStream(input)
+            .createScoped(List.of("https://www.googleapis.com/auth/spreadsheets"));
+
+        // 클라이언트 빌더 생성
+        return new Sheets.Builder(
+            GoogleNetHttpTransport.newTrustedTransport(),
+            JacksonFactory.getDefaultInstance(),
+            new HttpCredentialsAdapter(credentials)
+        ).setApplicationName("런치챗_이달의멘토").build();
+    }
 }
