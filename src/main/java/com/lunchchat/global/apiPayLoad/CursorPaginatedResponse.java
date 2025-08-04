b26dfbbc935100dba653478c83d7182df23fde44
lsw71311 <lsw71311@naver.com>
2025-08-04 23:51:19 +0900

    [FEAT] 커서 기반 페이징 클래스 생성

---
+++ src/main/java/com/lunchchat/global/apiPayLoad/CursorPaginatedResponse.java
@@ -0,0 +1,24 @@
+package com.lunchchat.global.apiPayLoad;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CursorPaginatedResponse<T> {
+    private List<T> data;
+    private CursorMeta meta;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class CursorMeta {
+        private int pageSize;
+        private boolean hasNext;
+        private String nextCursor; //프론트 반환
+    }
+
+}
