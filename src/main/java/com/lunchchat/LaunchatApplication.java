e474a9975ed011e05590d24032ef2ff3b32fa6d3
haerxeong <haeryeong04@naver.com>
2025-08-08 02:56:26 +0900

    [CHORE] AWS S3 의존성 교체

--- src/main/java/com/lunchchat/LaunchatApplication.java
+++ src/main/java/com/lunchchat/LaunchatApplication.java
@@ -1,11 +1,14 @@
 package com.lunchchat;
 
+import com.lunchchat.global.config.AwsS3Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties(AwsS3Properties.class)
 public class LaunchatApplication {
 
     public static void main(String[] args) {
