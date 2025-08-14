c5ec7e6214a19d2e74a59ec6604293c30ceef8ea
margie1a <4987kk@naver.com>
2025-08-14 10:56:31 +0900

    [REFACTOR] Repository 스캔 설정을 통한 부팅 성능 개선

--- src/main/java/com/lunchchat/LaunchatApplication.java
+++ src/main/java/com/lunchchat/LaunchatApplication.java
@@ -4,13 +4,26 @@ import com.lunchchat.global.config.AwsS3Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(AwsS3Properties.class)
+@EnableJpaRepositories(
+    basePackages = "com.lunchchat.domain",
+    excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MongoRepository.class)
+)
+@EnableMongoRepositories(
+    basePackages = "com.lunchchat.domain",
+    includeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MongoRepository.class)
+)
 public class LaunchatApplication {
 
     public static void main(String[] args) {
